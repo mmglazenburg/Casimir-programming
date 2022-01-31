1610bb510118eff6250f1dcb1943b3bca4932203
Marieke Glazenburg <m.m.glazenburg@tudelft.nl>
2022-01-31 13:44:19 +0100

    Add docstring to circumference function

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 print('hello world')
 
 def circumference(radius):
+    '''
+    Calculates the circumference of a circle given its radius.
+    '''
     return math.pi * radius * 2
 
 def area(radius):
